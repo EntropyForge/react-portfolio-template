01acc7b019daa07778b52cf97e4b159e47c03849
EntropyForge <noobcoder2307@gmail.com>
2026-04-22 18:01:53 +0530

    Update Project.tsx

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -10,19 +10,15 @@ function Project() {
             <div className="project">
                     <img src={CrewModuledata} className="zoom" alt="Hypersonic Crew Module Re-entry Analysis Collage" width="100%"/>
                 <h2>Crew Module </h2>
-                <p><strong>Objective:</strong><br/>Evaluate aerodynamic performance and structural integrity under extreme reentry conditions.</p>
-                <p><strong>Approach:</strong></p>
-                <ul>
-                    <li>Performed CFD analysis for hypersonic flow conditions (up to <strong>Mach 20</strong>).</li>
-                    <li>Mapped thermal and pressure distributions across the module surface.</li>
-                    <li>Conducted structural validation using pressure loads.</li>
-                </ul>
-                <p><strong>Key Results:</strong></p>
-                <ul>
-                    <li><strong>Drag coefficient:</strong> 1.8</li>
-                    <li><strong>Peak stagnation pressure:</strong> 7.9 MPa</li>
-                    <li><strong>Factor of Safety (FoS):</strong> 2.8 under hypersonic loads.</li>
-                </ul>
+               <p><strong>Objective:</strong><br/>Evaluate aerodynamic performance and structural integrity under extreme reentry conditions.</p>
+               <p><strong>Approach:</strong><br/>
+               • Performed CFD analysis for hypersonic flow conditions (up to <strong>Mach 20</strong>).<br/>
+               • Mapped thermal and pressure distributions across the module surface.<br/>
+               • Conducted structural validation using pressure loads.</p>
+               <p><strong>Key Results:</strong><br/>
+               • <strong>Drag coefficient:</strong> 1.8<br/>
+               • <strong>Peak stagnation pressure:</strong> 7.9 MPa<br/>
+               • <strong>Factor of Safety (FoS):</strong> 2.8 under hypersonic loads.</p>
             </div>
         </div>
     </div>
